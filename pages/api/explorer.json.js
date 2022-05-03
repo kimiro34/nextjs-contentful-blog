9ea3fb9e797ed1d337a5811acc51a3763cfd4a3d
Kimiro34 <ermoshinvladislav34@gmail.com>
2022-05-03 14:17:29 +0100

    add explorer data modified the catalyst node url to localhost

--- pages/api/explorer.json.js
+++ pages/api/explorer.json.js
@@ -1,6 +1,8 @@
+const config = require("./explorer.json");
+
 export default async (req, res) => {
   try {
-    return res.status(200).json({ error: null });
+    return res.status(200).json(config);
   } catch (error) {
     return res.status(400).json({
       error: `Oops, something went wrong...`,
